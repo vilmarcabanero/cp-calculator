250f50d425c29e4d71983f805f3cd19aaf630192
Vil Depanes <vilmarcabanero@gmail.com>
2023-12-05 03:16:41 +0800

    Set up MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, ChangeEvent } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface StatValues {
+  [key: string]: string;
 }
 
-export default App
+interface CPValues {
+  [key: string]: number;
+}
+
+const CombatPowerCalculator = () => {
+  const initialStats: StatValues = {
+    attack: '',
+    defense: '',
+    // ... add other stats here
+  };
+
+  const [stats, setStats] = useState<StatValues>(initialStats);
+  const [totalCP, setTotalCP] = useState(0);
+  const [autoClear, setAutoClear] = useState(true); // state for auto clear mode
+
+  const cpValues: CPValues = {
+    attack: 34.5,
+    defense: 21,
+    // ... add CP values for other stats
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setStats(prevStats => ({
+      ...prevStats,
+      [name]: value
+    }));
+  };
+
+  const calculateAndAddToTotalCP = () => {
+    let totalForCurrentInputs = 0;
+    for (const stat in stats) {
+      totalForCurrentInputs += (parseFloat(stats[stat]) || 0) * (cpValues[stat] || 0);
+    }
+    setTotalCP(prevTotalCP => prevTotalCP + totalForCurrentInputs);
+    if (autoClear) setStats(initialStats);
+  };
+
+  const resetCalculator = () => {
+    setStats(initialStats);
+    setTotalCP(0);
+  };
+
+  const toggleAutoClear = () => {
+    setAutoClear(!autoClear);
+  };
+
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Combat Power Calculator
+      </Typography>
+
+      <Button onClick={toggleAutoClear} variant="contained" color="primary">
+        {autoClear ? 'Switch to Manual Clear' : 'Switch to Auto Clear'}
+      </Button>
+
+      {Object.keys(initialStats).map(stat => (
+        <div key={stat}>
+          <TextField
+            label={stat.charAt(0).toUpperCase() + stat.slice(1)}
+            type="text"
+            id={stat}
+            name={stat}
+            value={stats[stat]}
+            onChange={handleInputChange}
+            margin="normal"
+            variant="outlined"
+          />
+        </div>
+      ))}
+
+      <Button onClick={calculateAndAddToTotalCP} variant="contained" color="secondary">
+        Calculate
+      </Button>
+      <Button onClick={resetCalculator} variant="contained">
+        Reset
+      </Button>
+
+      <Typography variant="subtitle1">
+        <strong>Total CP: {totalCP}</strong>
+      </Typography>
+    </div>
+  );
+};
+
+export default CombatPowerCalculator;
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    // ... other theme customizations
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
